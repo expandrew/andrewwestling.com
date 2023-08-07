e7b90a210b17fb4da0823394d9d1973fd762adfe
Andrew Westling <a@andrewwestling.com>
2023-08-06 23:45:43 -0400

    Move Media Cube into Projects folder
    Also move assets into a similar structure

--- next-js/next.config.js
+++ next-js/next.config.js
@@ -23,8 +23,7 @@ const autoLinkHeadingsOptions = {
         tagName: 'path',
         properties: {
           fillRule: 'evenodd',
-          d:
-            'M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z',
+          d: 'M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z',
         },
         children: [],
       },
@@ -49,8 +48,13 @@ module.exports = withMDX({
     return [
       {
         source: '/posts/media-cube',
-        destination: '/media-cube',
-        permanent: true,
+        destination: '/projects/media-cube',
+        permanent: false,
+      },
+      {
+        source: '/media-cube',
+        destination: '/projects/media-cube',
+        permanent: false,
       },
     ];
   },
