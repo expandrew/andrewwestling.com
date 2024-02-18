94a768535de56eb5832c54acc454d1e278092b71
Andrew Westling <a@andrewwestling.com>
2024-02-17 22:35:46 -0500

    Add Resume page back in
    - Update Resume generation command in task to point to new folder
    - Regenerate resume PDF and HTML using new prefixed classes
    - Add back the Resume route and components; add html.d.ts too
    - Fix resume template to not show empty `<li>` in contact section
    - Fix list item styling clash in resume CSS (explicitly set `list-style: disc` because Tailwind turns that off)

---
+++ next-js/app/resume/page.tsx
@@ -0,0 +1,23 @@
+import { Metadata } from "next";
+import { ResumeHtml } from "@components/ResumeHtml";
+import { ButtonLink } from "@components/Button";
+import resumeJson from "../../../resume/resume.json";
+
+export const metadata: Metadata = {
+  title: "Resume",
+  other: { date: resumeJson.meta.lastModified },
+};
+
+const Resume = () => (
+  <>
+    <div className="no-print">
+      <ButtonLink href="/assets/Andrew Westling Resume.pdf" target="_blank">
+        📃 Download as PDF
+      </ButtonLink>
+    </div>
+
+    <ResumeHtml />
+  </>
+);
+
+export default Resume;
