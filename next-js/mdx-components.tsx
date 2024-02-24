5e0de2820da2ae5ee3436c0d59cf276bf3a8d97c
Andrew Westling <a@andrewwestling.com>
2024-02-24 15:25:44 -0500

    Add custom styles for MDX elements
    This is just a starting point to get the HTML-ish defaults back
    
    Will probably extend this eventually

--- next-js/mdx-components.tsx
+++ next-js/mdx-components.tsx
@@ -2,6 +2,21 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: ({ children }) => (
+      <h1 className="font-semibold text-4xl">{children}</h1>
+    ),
+    h2: ({ children }) => (
+      <h2 className="font-semibold text-3xl">{children}</h2>
+    ),
+    h3: ({ children }) => (
+      <h3 className="font-semibold text-2xl">{children}</h3>
+    ),
+    h4: ({ children }) => <h4 className="font-semibold text-xl">{children}</h4>,
+    h5: ({ children }) => <h5 className="font-semibold text-lg">{children}</h5>,
+    h6: ({ children }) => <h6 className="font-semibold text-lg">{children}</h6>,
+    ul: ({ children }) => <ul className="ps-10 my-4">{children}</ul>,
+    li: ({ children }) => <li className="list-disc">{children}</li>,
+    p: ({ children }) => <p className="my-4">{children}</p>,
     ...components,
   };
 }
