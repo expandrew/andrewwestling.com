cbd91fbb3b59b31c38f283535a0eb476ff96f105
Andrew Westling <a@andrewwestling.com>
2024-02-27 00:32:15 -0500

    Remove old Next.js config

--- next-js-12/components/Greeting.tsx
+++
@@ -1,7 +0,0 @@
-import { Heading } from 'theme-ui';
-
-export const Greeting = () => (
-  <Heading as="h1" sx={{ fontSize: [2, 3], marginY: [3, 4] }}>
-    👋 Hi, I'm Andrew M Westling, a software engineer in Brooklyn, NY.
-  </Heading>
-);
