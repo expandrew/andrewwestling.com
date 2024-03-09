1bfc448c1758c7178427b050bc579a7e1d7d36dc
Andrew Westling <a@andrewwestling.com>
2024-03-09 17:51:44 -0500

    Fix autolinking on headings
    - Headings weren't getting IDs via remark-autolink-headings; realized this was because the `id=` prop wasn't getting through to the final element in HTML via the Markdown processor
    - The solution here is to spread the remaining props onto the MDX components; I went ahead and did it for all components but it was really only necessary for the headings for purposes of fixing the autolinking

--- next-js/mdx-components.tsx
+++ next-js/mdx-components.tsx
@@ -2,33 +2,60 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    h1: ({ children }) => (
-      <h1 className="font-semibold text-4xl my-6">{children}</h1>
+    h1: ({ children, ...props }) => (
+      <h1 {...props} className="font-semibold text-4xl my-6">
+        {children}
+      </h1>
     ),
-    h2: ({ children }) => (
-      <h2 className="font-semibold text-3xl my-6">{children}</h2>
+    h2: ({ children, ...props }) => (
+      <h2 {...props} className="font-semibold text-3xl my-6">
+        {children}
+      </h2>
     ),
-    h3: ({ children }) => (
-      <h3 className="font-semibold text-2xl my-6">{children}</h3>
+    h3: ({ children, ...props }) => (
+      <h3 {...props} className="font-semibold text-2xl my-6">
+        {children}
+      </h3>
     ),
-    h4: ({ children }) => (
-      <h4 className="font-semibold text-xl my-6">{children}</h4>
+    h4: ({ children, ...props }) => (
+      <h4 {...props} className="font-semibold text-xl my-6">
+        {children}
+      </h4>
     ),
-    h5: ({ children }) => (
-      <h5 className="font-semibold text-lg my-6">{children}</h5>
+    h5: ({ children, ...props }) => (
+      <h5 {...props} className="font-semibold text-lg my-6">
+        {children}
+      </h5>
     ),
-    h6: ({ children }) => (
-      <h6 className="font-semibold text-lg my-6">{children}</h6>
+    h6: ({ children, ...props }) => (
+      <h6 {...props} className="font-semibold text-lg my-6">
+        {children}
+      </h6>
     ),
-    ul: ({ children }) => <ul className="ps-10 list-revert">{children}</ul>,
-    ol: ({ children }) => <ol className="ps-10 list-revert">{children}</ol>,
-    p: ({ children }) => <p className="my-4">{children}</p>,
-    code: ({ children }) => (
-      <code className="bg-highlight dark:bg-highlight-dark text-primary rounded p-1">
+    ul: ({ children, ...props }) => (
+      <ul {...props} className="ps-10 list-revert">
+        {children}
+      </ul>
+    ),
+    ol: ({ children, ...props }) => (
+      <ol {...props} className="ps-10 list-revert">
+        {children}
+      </ol>
+    ),
+    p: ({ children, ...props }) => (
+      <p {...props} className="my-4">
+        {children}
+      </p>
+    ),
+    code: ({ children, ...props }) => (
+      <code
+        {...props}
+        className="bg-highlight dark:bg-highlight-dark text-primary rounded p-1"
+      >
         {children}
       </code>
     ),
-    hr: () => <hr className="my-6" />,
+    hr: ({ ...props }) => <hr {...props} className="my-6" />,
     ...components,
   };
 }
